4b9ea48fbf12cda5073af1333efa0d646ebde875
ericgaspar <junk.eg@free.fr>
2021-09-18 11:59:31 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+YNH_PHP_VERSION="7.4"
+
+# Composer version
+YNH_COMPOSER_VERSION="2.1.3"
+
+extra_php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-ldap"
 
 #=================================================
 # PERSONAL HELPERS
