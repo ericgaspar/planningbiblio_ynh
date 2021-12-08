b35f387d7ca21e2d731efac6a73f3862fcf195d7
ericgaspar <junk.eg@free.fr>
2021-12-08 15:17:38 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ YNH_PHP_VERSION="7.4"
 # Composer version
 YNH_COMPOSER_VERSION="2.1.3"
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-ldap"
+pkg_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-ldap"
 
 #=================================================
 # PERSONAL HELPERS
